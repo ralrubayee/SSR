5109d5045571315f99ddb4e2498f2bbb7578a5c8
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-11-04 12:58:41 -0500

    build profile index page

---
+++ routes/profiles.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { isLoggedIn } from '../middleware/middleware.js'
+import * as profilesCtrl from "../controllers/profiles.js"
+
+const router = Router()
+
+// localhost:3000/profiles - GET
+router.get("/", isLoggedIn, profilesCtrl.index)
+
+export {
+  router
+}
