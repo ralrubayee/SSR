480d7750c095ab34b375c56730193cbd038df9e1
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-11-04 09:00:00 -0500

    build taco index functionality

---
+++ controllers/tacos.js
@@ -0,0 +1,22 @@
+import { Taco } from "../models/taco.js"
+
+function index(req, res) {
+  // Find all tacos
+  Taco.find({})
+  // When we have all the tacos
+  .then(tacos => {
+    // Do something with the tacos
+    res.render("tacos/index", {
+      title: "🌮",
+      tacos,
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/tacos")
+  })
+}
+
+export {
+  index
+}
